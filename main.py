53324e57c243e61f3c4d4a9908a7df4eb76ae3ee
Anurag Kejriwal <anuragkejriwal1@gmail.com>
2024-06-02 22:51:35 -0400

    Added more functionality

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ from flask_restful import Api, Resource
 
 app = Flask(__name__)
 
-#wrap app in a RESTful API
+# wrap app in a RESTful API
 api = Api(app)
 
-#inherit Resource to handle GET, PUT, etc.
+names = {
+    "Anurag": {"age": 18, "gender": "male"},
+    "Batman": {"age": 43, "gender": "male"},
+}
+
+
+# inherit Resource to handle GET, PUT, etc.
 class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
-    
-api.add_resource(HelloWorld, "/helloworld")
+    def get(self, name):
+        return names[name]
+
+    def post(self):
+        return {"data": "Posted"}
+
+
+# string name parameter to be passed in after helloworld
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
